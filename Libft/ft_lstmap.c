670b34f1c7f197f4b926a0321b50f666c15a5c23
Yegi Park <yehji205@gmail.com>
2023-06-09 00:38:13 +0200

    ft_lstmap adding codes

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yegpark <yegpark@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: yegipark <yegipark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 18:18:20 by yegpark           #+#    #+#             */
-/*   Updated: 2023/06/08 18:21:15 by yegpark          ###   ########.fr       */
+/*   Updated: 2023/06/09 00:37:38 by yegipark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,24 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	if (!lst)
-		return (0);
-	
+	t_list	*new_list;
+	t_list	*current;
+	t_list	*new_node;
+	void	*new_content;
+
+	new_list = 0;
+	current = lst;
+	while (current)
+	{
+		new_content = f(current->content);
+		new_node = ft_lstnew(new_content);
+		if(!new_node)
+		{
+			ft_lstclear(&new_list, del);
+			return (0);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		current = current->next;
+	}
+	return (new_list);
 }
